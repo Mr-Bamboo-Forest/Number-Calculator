a5f9ee0b52c07de0c6dce7f04a456fc640fe9f4e
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2022-10-26 08:10:06 +1000

    Simple changes
    made entries into numbers only and added tetrahedral numbers

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 //Square Number Calculate
 function squarecalc(){
-    var squaresolveanswer = document.getElementById("squaresolve").value * document.getElementById("squaresolve").value;
+    var squaresolveanswer = Number(document.getElementById("squaresolve").value * document.getElementById("squaresolve").value);
     document.getElementById("squareanswer").innerText = squaresolveanswer;  
 }
 
 //Cube Number Calculate
 function cubecalc(){
-    var cubesolveanswer = document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value;
+    var cubesolveanswer = Number(document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value);
     document.getElementById("cubeanswer").innerText = cubesolveanswer;  
 }
 //Triangle Number Calculate
 function trianglecalc(){
-    var trianglesolveanswer = document.getElementById("trianglesolve").value;
+    var trianglesolveanswer = Number(document.getElementById("trianglesolve").value);
     var trianglenumber = trianglesolveanswer; // 1
     var trianglenumberadd1 = trianglenumber; 
     trianglenumberadd1 ++;
     var trianglenumber2 = trianglenumber * trianglenumberadd1; //3
     trianglenumber = trianglenumber2 /2;//1.5
     document.getElementById("triangleanswer").innerText = trianglenumber;  
+}
+//Tetrahedral Number Calculate
+function tetrahedralcalc(){
+    var tetrahedralsolveanswer = Number(document.getElementById("tetrahedralsolve").value);
+    var t = tetrahedralsolveanswer;
+    var tetrahedraladd1 = t+=1;
+    var tetrahedraladd2 = tetrahedraladd1 +=1;
+    tetrahedraladd1 --;
+    var tetrahedraladd3 = tetrahedraladd2 +=1;
+    tetrahedraladd2 --;
+    var tetrahedral = tetrahedraladd1 * tetrahedraladd2 * tetrahedraladd3;
+    var tetrahedrald6 = tetrahedral /6;
+    document.getElementById("tetrahedralanswer").innerText = tetrahedrald6;  
 }
