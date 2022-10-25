a25028a499969dbbca2d41a09737acab867820f9
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2022-10-25 14:53:56 +1000

    Triangular Numbers Solver

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //Square Number Calculate
 function squarecalc(){
-    let squaresolveanswer = document.getElementById("squaresolve").value * document.getElementById("squaresolve").value;
+    var squaresolveanswer = document.getElementById("squaresolve").value * document.getElementById("squaresolve").value;
     document.getElementById("squareanswer").innerText = squaresolveanswer;  
 }
 
 //Cube Number Calculate
 function cubecalc(){
-    let cubesolveanswer = document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value;
+    var cubesolveanswer = document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value;
     document.getElementById("cubeanswer").innerText = cubesolveanswer;  
+}
+//Triangle Number Calculate
+function trianglecalc(){
+    var trianglesolveanswer = document.getElementById("trianglesolve").value;
+    var trianglenumber = trianglesolveanswer; // 1
+    var trianglenumberadd1 = trianglenumber; 
+    trianglenumberadd1 ++;
+    var trianglenumber2 = trianglenumber * trianglenumberadd1; //3
+    trianglenumber = trianglenumber2 /2;//1.5
+    document.getElementById("triangleanswer").innerText = trianglenumber;  
 }
