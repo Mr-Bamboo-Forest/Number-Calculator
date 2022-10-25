00b0781cff07eb012f173ca48b9ac0c64bd67c68
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2022-10-25 12:16:22 +1000

    little updates

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//Square Number Calculate
 function squarecalc(){
     let squaresolveanswer = document.getElementById("squaresolve").value * document.getElementById("squaresolve").value;
     document.getElementById("squareanswer").innerText = squaresolveanswer;  
 }
 
+//Cube Number Calculate
 function cubecalc(){
     let cubesolveanswer = document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value;
     document.getElementById("cubeanswer").innerText = cubesolveanswer;  
