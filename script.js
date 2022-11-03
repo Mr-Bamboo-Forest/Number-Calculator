8bc772a066e73d0bf8d057f76aa6f50b91070715
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2022-11-04 09:07:19 +1000

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Square Number Calculate
 function squarecalc(){
-    var squaresolveanswer = Number(document.getElementById("squaresolve").value ^ 2);
+    var squaresolveanswer = Number(document.getElementById("squaresolve").value * Number(document.getElementById("squaresolve").value));
     document.getElementById("squareanswer").innerText = comma(squaresolveanswer);  
 }
 //Cube Number Calculate
 function cubecalc(){
-    var cubesolveanswer = Number(document.getElementById("cubesolve").value ^ 3);
+    var cubesolveanswer = Number(document.getElementById("cubesolve").value * document.getElementById("cubesolve").value * document.getElementById("cubesolve").value);
     document.getElementById("cubeanswer").innerText = comma(cubesolveanswer);  
 }
 //Triangle Number Calculate
