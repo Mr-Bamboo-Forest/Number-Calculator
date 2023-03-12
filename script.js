90f4e2dcee67b50d72d2c93046962b40267995e3
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2023-03-13 09:09:02 +1000

    q

--- script.js
+++ script.js
@@ -120,8 +120,9 @@ function calculateloan() {
     var interest = (amount * (rate * 0.01)) / months;
     var totalamount = ((amount / months) + interest).toFixed(2);
     var totalamountwithoutinterest = ((amount / months)).toFixed(2);
-    document.getElementById("monthlypayment").innerText = ("Interest is ") + interest + (" and monthly payment (2 d.p., and with interest) is ") + totalamount;
-    document.getElementById("monthlypaymentwithoutinterest").innerText = ("Monthly payment (2 d.p. and without interest) is ") + totalamountwithoutinterest;
+    document.getElementById("interest").innerText = ("Interest is $") + interest;
+    document.getElementById("monthlypayment").innerText = ("Monthly payment (2 d.p., and with interest) is $") + totalamount;
+    document.getElementById("monthlypaymentwithoutinterest").innerText = ("Monthly payment (2 d.p. and without interest) is $") + totalamountwithoutinterest;
 };
 
 //comma
