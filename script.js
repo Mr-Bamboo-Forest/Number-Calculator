f4da50d54cd3aca9a722fb43e8167b3a6915d81f
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-01-18 13:42:37 +1000

    Created a search bar at top

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// JavaScript code
+//Search Function 
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('subjects');
+    
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display = "none";
+        }
+        else {
+        x[i].style.display = "list-item";
+        }
+    }
+}
+    
+
 //Square Number Calculate
 function squarecalc(){
     var squaresolveanswer = Number(document.getElementById("squaresolve").value * Number(document.getElementById("squaresolve").value));
